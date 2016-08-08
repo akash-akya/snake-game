3e42b04ac1514fe83cefd3fc360cbf399f543da5
akash akya <akashh246@gmail.com>
2016-08-08 22:32:12 +0530

    Coding style fix

--- src/display_util.h
+++ src/display_util.h
@@ -5,14 +5,23 @@
 #define Y_MAX 37
 
 int set_unit_size (int unit_x, int unit_y);
+
 void print_char_at_point (int pos_y, int pos_x, char ch);
+
 void print_string_at_point (int pos_y, int pos_x, char ch, int length);
-int get_x_max();
-int get_y_max();
-int get_mid_x();
-int get_mid_y();
-int get_unit_size();
+
 void display_delay(long refresh_rate);
+
 void replace_all_char(char src, char dst);
+
+int get_x_max();
+
+int get_y_max();
+
+int get_mid_x();
+
+int get_mid_y();
+
+int get_unit_size();
 
 #endif
